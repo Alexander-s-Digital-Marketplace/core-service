07d7c687d54033f09e7060604c2f389531a4ed17
alexander kusakin <pm12.kusakin@gmail.com>
2025-04-10 19:31:10 +0700

    ./internal/handler/: add product default settings

--- internal/handlers/create_product/create_product.go
+++ internal/handlers/create_product/create_product.go
@@ -1,17 +1,29 @@
 package createproduct
 
 import (
+	"time"
+
 	productmodel "github.com/Alexander-s-Digital-Marketplace/core-service/internal/models/product_model"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateProduct(c *gin.Context) (int, string) {
+	id, exists := c.Get("id")
+	if !exists {
+		return 400, "Bad request"
+	}
+
 	var product productmodel.Product
 	var code int
 	code = product.DecodeFromContext(c)
 	if code != 200 {
 		return code, "Error decode JSON"
 	}
+
+	product.PubDate = time.Now().Format("2006-01-02 15:04")
+	product.SellerId = id.(int)
+	product.IsBuy = false
+	product.IsSellNow = false
 
 	code = product.AddToTable()
 	if code != 200 {
